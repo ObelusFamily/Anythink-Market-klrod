472c8eede616a2bb7aa43002b2dc12639e89515d
Daniel Lee <daniel.lee@databricks.com>
2022-08-15 19:16:06 -0400

    lint

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -35,8 +35,9 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        imgSrc={process.env.PUBLIC_URL + "/placeholder.png"}
-        src={item.image ? item.image : process.env.PUBLIC_URL + "/placeholder.png"}
+        src={
+          item.image ? item.image : process.env.PUBLIC_URL + "/placeholder.png"
+        }
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
