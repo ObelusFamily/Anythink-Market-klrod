d77f9b8f696eb0caa60f682dee43322940df6b1b
Daniel Lee <daniel.lee@databricks.com>
2022-08-15 18:17:43 -0400

    requiring png instead

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import PlaceHolderImage from '../imgs/placeholder.png'
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -29,7 +28,7 @@ const ItemPreview = (props) => {
       props.favorite(item.slug);
     }
   };
-  console.log(item.image)
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -37,7 +36,9 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image !== "" ? item.image : PlaceHolderImage}
+        src={
+          item.image !== "" ? item.image : require("../imgs/placeholder.png")
+        }
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
